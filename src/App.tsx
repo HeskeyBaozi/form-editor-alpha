4b5f3893344a36a72e55d50f46dec95b86f02cc5
HeZhiyu <zhiyuhe@sfmail.sf-express.com>
2019-10-22 11:07:18 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import styled from "styled-components";
+import FormEditor from "./components/FormEditor";
+import "./App.css";
+
+const PreviewWrapper = styled.div`
+  border: 1px solid lightblue;
+  width: 75vw;
+`;
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <PreviewWrapper>
+          <FormEditor />
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </PreviewWrapper>
       </header>
     </div>
   );
-}
+};
 
 export default App;
